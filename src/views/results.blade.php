7e06b0c812a148d36bea25493415107bc1540edc
Alex Dover <me@alexdover.co.uk>
2014-05-23 14:46:06 +0100

    Changed template to display empty result set when not query is being run.

--- src/views/results.blade.php
+++ src/views/results.blade.php
@@ -8,38 +8,37 @@
 	<button type="submit" class="btn btn-default">Search</button>
 </form>
 
-@if ($results->count() > 0)
-	<h2>Results for "{{ $query }}" ({{ $results->getTotal() }})</h2>
+@if ($query)
 
-	<table class="table table-striped">
-		@foreach ($results as $hit)
-			<tr>
-				<td>
-					@if (isset($hit['_source']['url']))
-						<a href="{{ $hit['_source']['url'] }}">{{ $hit['_source']['name'] }}</a>
-					@else
-						{{ $hit['_source']['name'] }}
-					@endif
-				</td>
-				<td>Score (debug): {{ $hit['_score'] }}</td>
-				{{--
-				<td>
-					<pre>{{ var_export($hit['_source']) }}</pre>
-				</td>
-				--}}
-			</tr>
-		@endforeach
-	</table>
+	@if ($results->count() > 0)
+		<h2>Results for "{{ $query }}" ({{ $results->getTotal() }})</h2>
 
-	{{ $results->appends(['q' => $query])->links() }}
-@else
+		<table class="table table-striped">
+			@foreach ($results as $hit)
+				<tr>
+					<td>
+						@if (isset($hit['_source']['url']))
+							<a href="{{ $hit['_source']['url'] }}">{{ $hit['_source']['name'] }}</a>
+						@else
+							{{ $hit['_source']['name'] }}
+						@endif
+					</td>
+					<td>Score (debug): {{ $hit['_score'] }}</td>
+					{{--
+					<td>
+						<pre>{{ var_export($hit['_source']) }}</pre>
+					</td>
+					--}}
+				</tr>
+			@endforeach
+		</table>
 
-	@if ($query == '')
-		<h2>Error</h2>
-		<p>Please enter a query.</p>
+		{{ $results->appends(['q' => $query])->links() }}
 	@else
+
 		<h2>No results for "{{ $query }}"</h2>
 		<p>Please try again with a different query.</p>
+
 	@endif
 
 @endif
