b3e8236e5d9047fc73af29b59db4b341ea164c72
Alex Dover <me@alexdover.co.uk>
2014-12-12 14:42:20 +0000

    Improvements to the search provider - it is now possible to add page types to the results, which will narrow down the results. Also, by adding a json parameter to the URL it is possible to return the results as json.

--- src/CoandaCMS/ElasticSearch/CoandaElasticSearchProvider.php
+++ src/CoandaCMS/ElasticSearch/CoandaElasticSearchProvider.php
@@ -3,22 +3,20 @@
 use CoandaCMS\Coanda\Search\CoandaSearchProvider;
 
 use Elasticsearch, Log, Config, Input, View, Paginator, Coanda;
+use Elasticsearch\Common\Exceptions\Curl\CouldNotConnectToHost;
+use Elasticsearch\Common\Exceptions\Missing404Exception;
 
-/**
- * Class CoandaElasticSearchProvider
- * @package CoandaCMS\ElasticSearch
- */
 class CoandaElasticSearchProvider implements CoandaSearchProvider {
 
-    /**
-     * @var
+	/**
+	 * @var
      */
-    private $client;
+	private $client;
 
-    /**
-     *
+	/**
+	 *
      */
-    private function initClient()
+	private function initClient()
 	{
 		$hosts = Config::get('coanda-elastic-search::elastic.hosts');
 
@@ -28,10 +26,10 @@ class CoandaElasticSearchProvider implements CoandaSearchProvider {
 		$this->client = new Elasticsearch\Client($params);
 	}
 
-    /**
-     * @return string
+	/**
+	 * @return string
      */
-    private function indexName()
+	private function indexName()
 	{
 		$index_name = Config::get('coanda-elastic-search::elastic.index_name');
 
@@ -43,6 +41,9 @@ class CoandaElasticSearchProvider implements CoandaSearchProvider {
 		return $index_name;
 	}
 
+	/**
+	 * @return mixed
+     */
 	public function addPagesIndex()
 	{
 		$this->initClient();
@@ -52,6 +53,9 @@ class CoandaElasticSearchProvider implements CoandaSearchProvider {
 		return $result;
 	}
 
+	/**
+	 * @return mixed
+     */
 	public function setupMappings()
 	{
 		// Set the index and type
@@ -86,13 +90,14 @@ class CoandaElasticSearchProvider implements CoandaSearchProvider {
 		return $result;
 	}
 
-    /**
-     * @param $module
-     * @param $module_id
-     * @param $url
-     * @param $search_data
-     */
-    public function register($module, $module_id, $url, $search_data)
+	/**
+	 * @param $module
+	 * @param $module_id
+	 * @param $url
+	 * @param $search_data
+	 * @return mixed|void
+	 */
+	public function register($module, $module_id, $url, $search_data)
 	{
 		$this->initClient();
 
@@ -103,7 +108,7 @@ class CoandaElasticSearchProvider implements CoandaSearchProvider {
 
 		$search_data['coanda_url'] = $url;
 
-		// If we don't have visibilty dates, then set a pretty generous default!
+		// If we don't have visibility dates, then set a pretty generous default!
 		if (!isset($search_data['visible_from']))
 		{
 			$search_data['visible_from'] = '2000-01-01 12:00:00';
@@ -119,11 +124,12 @@ class CoandaElasticSearchProvider implements CoandaSearchProvider {
 		$this->client->index($index_params);
 	}
 
-    /**
-     * @param $module
-     * @param $module_id
-     */
-    public function unRegister($module, $module_id)
+	/**
+	 * @param $module
+	 * @param $module_id
+	 * @return mixed|void
+	 */
+	public function unRegister($module, $module_id)
 	{
 		try
 		{
@@ -136,25 +142,24 @@ class CoandaElasticSearchProvider implements CoandaSearchProvider {
 
 			$this->client->delete($delete_params);
 		}
-		catch (\Elasticsearch\Common\Exceptions\Missing404Exception $exception)
+		catch (Missing404Exception $exception)
 		{
 			Log::info('Elastic search, failed to remove: ' . $module . ' -> ' . $module_id);
 		}
 	}
 
-    /**
-     * @return mixed
+	/**
+	 * @return mixed
      */
-    public function handleSearch()
+	public function handleSearch()
 	{
 		$query = Input::has('q') ? Input::get('q') : false;
-
 		$page = Input::has('page') ? Input::get('page') : 1;
 		$per_page = 10;
 		$results_template = Config::get('coanda-elastic-search::elastic.results_template');
 
 		$total = false;
-		$results = false;
+		$results = [];
 
 		if (!$query)
 		{
@@ -172,17 +177,51 @@ class CoandaElasticSearchProvider implements CoandaSearchProvider {
 			$query_params['from'] = $offset;
 			$query_params['size'] = $per_page;
 
-			$filter = array();
+			$filter = [
+				'and' => [
+					'filters' => [
+						[
+							'range' => [
+								'visible_from' => [
+									'lt' => date('Y-m-d H:m:s', time())
+								]
+							]
+						],
+						[
+							'range' => [
+								'visible_to' => [
+									'gt' => date('Y-m-d H:m:s', time())
+								]
+							]
+						],
+					]
+				]
+			];
 
-			$filter['and']['filters'][0]['range']['visible_from']['lt'] = date('Y-m-d H:m:s', time());
-			$filter['and']['filters'][1]['range']['visible_to']['gt'] = date('Y-m-d H:m:s', time());
+			$page_type_string = Input::has('type') ? Input::get('type') : '';
+			$page_types = [];
 
-			$_query = array();
-			$_query['match']['_all'] = $query;
+			if ($page_type_string != '')
+			{
+				$page_types = explode(',', $page_type_string);
+			}
+
+			if (count($page_types) > 0)
+			{
+				$filter['and']['filters'][] = [
+					'terms' => [
+							'page_type' => $page_types
+						]
+				];
+			}
 
 			$query_params['body']['query']['filtered'] = array(
-			    'filter' => $filter,
-			    'query'  => $_query
+				'filter' => $filter,
+				'query'  => [
+					'match' => [
+						'_all' => $query
+					]
+				]
 			);
 
 			try
@@ -192,17 +231,22 @@ class CoandaElasticSearchProvider implements CoandaSearchProvider {
 				$results = $search_results['hits']['hits'];
 				$total = $search_results['hits']['total'];
 			}
-			catch (\Elasticsearch\Common\Exceptions\Missing404Exception $exception)
-			{
-				$results = [];
+			catch (Missing404Exception $exception)
+ 			{
+				// empty results
 			}
-			catch (\Elasticsearch\Common\Exceptions\Curl\CouldNotConnectToHost $exception)
+			catch (CouldNotConnectToHost $exception)
 			{
-				$results = [];
+				// empty results
 			}
 		}
 
 		$paginated_results = Paginator::make($results, $total, $per_page);
+
+		if (Input::get('format') == 'json')
+		{
+			return $paginated_results;
+		}
 
 		$layout = Coanda::module('layout')->layoutFor('search:results');
 
@@ -213,6 +257,9 @@ class CoandaElasticSearchProvider implements CoandaSearchProvider {
 				'description' => ''
 			],
 			'layout' => $layout,
+			'breadcrumb' => [
+				'search' => 'Search'
+			],
 			'module' => 'search',
 			'module_identifier' => 'results'
 		];
